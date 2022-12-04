a51017501b13412a048329206891201cf80d0407
alexanderDeb <jhon.carvajal01@unicatolica.edu.co>
2022-12-04 15:38:23 -0500

    deploy version

--- src/Pages/homePage.js
+++ src/Pages/homePage.js
@@ -1,60 +1,121 @@
-import React from 'react'
-import Hero from '../Assets/Landig_image.png'
-import Card_1 from '../Assets/Landing_card2.png'
-import Card_2 from '../Assets/Landig_card_2.png'
+import React from "react";
+import HeroImg from "../Assets/Home_Hero_Img.jpeg";
+import Card_1 from "../Assets/Card_Img_Principal.jpg";
+import Card_2 from "../Assets/Card_img.jpeg";
+import {FiMail, FiPhoneOutgoing} from "react-icons/fi"
 
 export default function HomePage() {
   return (
     <div>
-      <div className="hero min-h-screen" style={{ backgroundImage: `url(${Hero})` }}>
-        <div className="hero-overlay bg-opacity-60"></div>
+      <div
+        className="flex justify-center bg-sky-600 md:hidden bg-contain bg-repeat"
+        style={{ backgroundImage: `url(${HeroImg})`, backgroundPosition: 'center'}}
+      >
+        <div className="hero-overlay bg-opacity-60 text-white">
+          <h3 className="text-4xl pt-10">M&M DRYWALL CONSTRUCTION LLC</h3>
+          <p className="my-5">
+            This is a company focused on metal framing which is always looking
+            for customer satisfaction.{" "}
+          </p>
+        </div>
+      </div>
+      <div
+        className=" hidden md:hero md:min-h-screen md:bg-contain md:bg-repeat md:h-full md:w-full"
+        style={{ backgroundImage: `url(${HeroImg})` }}
+      >
+        <div className="hero-overlay bg-opacity-60" />
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">M&M DRYWALL CONSTRUCTION LLC</h1>
-            <p className="mb-5">This is a company focused on metal framing which is always looking for customer satisfaction. </p>
-            <button className="btn btn-primary">Get Started</button>
+            <h1 className="mb-5 text-5xl font-bold">
+              M&M DRYWALL CONSTRUCTION LLC
+            </h1>
+            <p className="mb-5">
+              This is a company focused on metal framing which is always looking
+              for customer satisfaction.{" "}
+            </p>
           </div>
         </div>
       </div>
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row">
-          <img src={Card_1} className="max-w-sm rounded-lg shadow-2xl" />
-          <div>
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
-            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-            <button className="btn btn-primary">Get Started</button>
+      <div className="md:hero md:min-h-screen bg-base-200">
+        <div className="md:hero-content flex-col lg:flex-row">
+          <img
+            src={Card_1}
+            className="max-w-sm rounded-lg shadow-2xl hidden md:flex"
+          />
+          <div className="p-2 md:p-0 md:pl-5">
+            <h1 className="pt-20 text-4xl md:text-5xl md:pt-0 font-bold">
+              M&M DRYWALL CONSTRUCTION LLC
+            </h1>
+            <p className="py-6">
+              With a clear focus on supplying local, quality building materials
+              for new construction and an unmatched level of service to
+              homebuilders and other large project customers. Our 30 years of
+              excellent service demonstrates our consistency and passion for
+              what we do.
+            </p>
           </div>
         </div>
       </div>
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <img src={Card_2} className="max-w-sm rounded-lg shadow-2xl" />
-          <div>
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
-            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-            <button className="btn btn-primary">Get Started</button>
+      <div className="md:hero md:min-h-screen bg-base-200">
+        <div className="md:hero-content flex-col lg:flex-row-reverse">
+          <img
+            src={Card_2}
+            className="max-w-sm rounded-lg shadow-2xl hidden md:flex"
+          />
+          <div className="p-2 md:p-0">
+            <h1 className="pt-20 text-4xl md:text-5xl md:pt-0 font-bold">
+              M&M DRYWALL CONSTRUCTION LLC
+            </h1>
+            <p className="py-6">
+              Give us the opportunity to show you the difference. Call us with
+              your needs and let us schedule a delivery for you.
+            </p>
           </div>
         </div>
       </div>
-      <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
-        <div className="grid grid-flow-col gap-4">
-          <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
+      <footer className="footer p-10 bg-base-300 text-base-content">
+        <div>
+          <span className="footer-title">Services</span>
+          <p><span className="font-semibold">Email:</span> masaldo1220@gmail.com</p>
+          <p><span className="font-semibold">Number: </span>(252) 412-4185</p>
+          <p><span className="font-semibold">Personal Email: </span>Martaqq33@gmail.com</p>
         </div>
         <div>
+          <span className="footer-title">Social</span>
           <div className="grid grid-flow-col gap-4">
-            <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a>
-            <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
-            <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
+            <a href="mailto:masaldo1220@gmail.com" className="text-2xl">
+              <FiMail/>
+            </a>
+            <a href="tel:(252) 412-4185" className="text-2xl">
+              <FiPhoneOutgoing/>
+            </a>
+            <a href="mailto:Martaqq33@gmail.com" className="text-2xl">
+            <FiMail/>
+            </a>
           </div>
-        </div>
-        <div>
-          <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
         </div>
       </footer>
+      {/* <footer className="footer footer-center p-10 bg-base-200 text-base-content">
+        <div>
+          <div className="grid grid-flow-col gap-4">
+            <a href="mailto:masaldo1220@gmail.com" className="link link-hover">
+              Contact Email
+            </a>
+            <a href="tel:(252) 412-4185" className="link link-hover">
+              Contact Number
+            </a>
+            <a href="mailto:Martaqq33@gmail.com" className="link link-hover">
+              Personal contact
+            </a>
+          </div>
+          <div>
+            <p><span className="font-semibold">Email:</span> masaldo1220@gmail.com</p>
+            <p><span className="font-semibold">Number: </span>(252) 412-4185</p>
+            <p><span className="font-semibold">Personal Email: </span>Martaqq33@gmail.com</p>
+          </div>
+          <p>Copyright © 2022 - All right reserved</p>
+        </div>
+      </footer> */}
     </div>
-
-  )
+  );
 }
